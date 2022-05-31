e6d1da0b09a1417f889780a5d207a66f295495da
junaidapdev <junaidap.dev.com>
2022-05-31 10:59:53 +0530

    homepage structure added

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+      <div className="Home">
+        <div className="profileSide">Profile</div>
+        <div className="postSide">Post</div>
+        <div className="rightSide">Trends</div>
+     </div>
+    )
+}
+
+export default Home
